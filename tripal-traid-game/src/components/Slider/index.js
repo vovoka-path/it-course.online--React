47d305c49ade96daed70c6bd04b3e50d7c293c78
vovoka-path <vovoka.path@gmail.com>
2022-04-15 19:27:12 +0300

    refactor: Slider (add Container etc.)

--- tripal-traid-game/src/components/Slider/index.js
+++ tripal-traid-game/src/components/Slider/index.js
@@ -1,19 +1,26 @@
-import style from './Slider.module.css';
+import cn from "classnames";
+import Container from '../Container';
+import Heading from "../Heading";
+import s from "./Slider.module.css";
 
 const Slider = () => {
   return (
-    <section className={style.section}>
-      <div className={style.slider}>
-        <div className={style.container + " " + style.sliderContent}>
-          <h1 className={style.header}>Wow</h1>
-          <h2 className={style.subheader}>Wow.Wow.Wow</h2>
-          <div className={style.call}>
-            <button className={style.button}>Wow</button>
+    <section className={s.section}>
+      <div className={s.slider}>
+        <Container className={cn(s.container, s.sliderContent)}>
+          <Heading level={1} className={s.header}>
+            Wow
+          </Heading>
+          <Heading level={2} className={s.subheader}>
+            Wow.Wow.Wow
+          </Heading>
+          <div className={s.call}>
+            <button className={s.button}>Wow</button>
           </div>
+        </Container>
         </div>
-      </div>
     </section>
-  )
+  );
 };
 
 export default Slider;
