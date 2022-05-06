e57130bd497a73e0c533fa74fee3a3abf3e2b2e3
vovoka-path <vovoka.path@gmail.com>
2022-05-06 15:54:10 +0300

    fix: Button

--- tripal-traid-game/src/components/Button/Button.js
+++ tripal-traid-game/src/components/Button/Button.js
@@ -1,39 +1,39 @@
 import PropTypes from "prop-types";
 import cn from "classnames";
 
+import s from "./Button.module.scss";
 
-const Button = (props) => {
-  const {
-    className, 
-    buttonText,
-    isSliderButtonClick,
-  } = props;
 
+const Button = ({
+  color,
+  children,
+  onClick
+}) => {
+  
   const handleButtonClick = () => {
-    if (buttonText === 'Go back') {
-      const isShowMiniSlider = true;
-      isSliderButtonClick(isShowMiniSlider);
-    }
+    onClick && onClick();
   };
 
   return (
     <button 
-      className={cn(className)}
+      className={cn(s.root, s[color])}
       onClick={handleButtonClick}
     >
-      {buttonText}
+      {children}
     </button>
   );
 };
 
 Button.propTypes = {
+  color: PropTypes.oneOf(['default', 'dark']),
+  children: PropTypes.node,
   className: PropTypes.string,
   buttonText: PropTypes.string,
   handleButtonClick: PropTypes.func,
 };
 
 Button.defaultProps = {
-  buttonText: 'Wow',
+  color: 'default',
 }
 
 export default Button;
