1e53f3446d42e0ffae8e2c1e83be10639ebb16f4
vovoka-path <vovoka.path@gmail.com>
2022-05-07 00:04:36 +0300

    feat: About

---
+++ tripal-traid-game/src/pages/About/About.js
@@ -0,0 +1,29 @@
+import Container from "../../components/Container";
+import Text from "../../components/Text";
+import Heading from "../../components/Heading";
+
+import s from "./About.module.scss";
+
+import { ABOUT } from "../../data/about";
+
+
+function About() {
+
+  return (
+    <section className={s.cardSection}>
+      <Container>
+        <div className={s.cardTittle}>
+          <Heading level={2} backline>About Game</Heading>
+        </div>
+        {ABOUT.map( (paragraph, index) => {
+          const key = index;
+
+          return <Text key={key} element="p">{paragraph.text}</Text>
+        })
+        }
+      </Container>
+    </section>
+  );
+}
+
+export default About;
