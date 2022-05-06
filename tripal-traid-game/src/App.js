f00930b004f293da2c68a4d86c2b451674063df3
vovoka-path <vovoka.path@gmail.com>
2022-05-06 15:55:30 +0300

    fix: App.js + scss

--- tripal-traid-game/src/App.js
+++ tripal-traid-game/src/App.js
@@ -1,65 +1,65 @@
 import { useState } from "react";
 
+import Container from "./components/Container";
 import Header from "./components/Header";
 import Slider from "./components/Slider";
 import Footer from "./components/Footer";
-import Cards from "./components/Cards";
 import Biography from "./pages/Biography";
+import Heading from "./components/Heading";
+import CharacterCard from "./components/CharacterCard";
+
+import s from "./App.module.scss";
+
+import { CHARACTER } from "./data/characterCards";
 
 
 const App = () => {
-  const [ 
-    bioData, 
-    setBioData 
-  ] = useState({
-    'id': 0,
-    'isShow': false,
-  });
+  const [character, setCharacter] = useState(CHARACTER);
+  const [characterID, setCharacterID] = useState(null);
 
-  let content;
-
-  togglePage(bioData.isShow);
-
-  function togglePage(isShowBio) {
-    switch (isShowBio) {
-      case false:
-        content = (
-          <Cards 
-            onBioClick={handleBioClickInApp} 
-          />
-        )
-        break;
-      case true:
-        content = (
-          <Biography 
-            id={bioData.id}
-          />
-        )
-        break;   
-      default:
-        break;
-    }
-  }
-
-  function handleBioClickInApp(data) {
-    setBioData(data);
+  const handleLikeClick = (id) => {
+    setCharacter((prevState) =>
+      prevState.map((item) =>
+        item.id === id ? { ...item, isLike: !item.isLike } : item
+      )
+    );
   };
-  
-  function handleGoBackButtonClick(isButtonClick) {
-    const data = {...bioData, isShow: isButtonClick};
 
-    setBioData(data);
-    togglePage(data);
-  }
+  const handleBioClick = (id) => {
+    setCharacterID(id);
+  };
 
   return (
     <>
       <Header />
-      <Slider 
-        goBackButtonClick={handleGoBackButtonClick} 
-        isShowFullSlider={bioData.isShow}
-      />
-      {content}
+      {
+        characterID !== null ?
+        <Biography id={characterID} onBackClick={() => setCharacterID(null)} /> :
+        <>
+          <Slider />
+          <section className={s.cardSection}>
+            <Container>
+              <div className={s.cardTittle}>
+                <Heading backline>Marvel Cards</Heading>
+                <Heading level={2}>Collect your best five</Heading>
+              </div>
+              <div className={s.cardWrap}>
+                {character.map((item) => {
+                  return (
+                    <div key={item.id}>
+                      <CharacterCard 
+                        character={item} 
+                        onLikeClick={handleLikeClick}
+                        onBioClick={handleBioClick}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            </Container>
+          </section>
+        </>
+      }
       <Footer />
     </>
   );
